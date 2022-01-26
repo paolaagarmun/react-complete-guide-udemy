4942a208ba11cfb0bdb40fb91ca38dbbce27b46c
paolaagarmun <paola.a.garciamunoz@gmail.com>
2022-01-25 22:05:09 -0500

    changed expenses display as a map function

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -3,23 +3,37 @@ import './Expenses.css'
 
 function Expenses(props) {
     return (
-        <div className="expenses">
-             <ExpenseItem
-            title={props.items[0].title}
-            amount={props.items[0].amount}
-            date={props.items[0].date}
-            />
-            <ExpenseItem
-            title={props.items[1].title}
-            amount={props.items[1].amount}
-            date={props.items[1].date}
-            />
-            <ExpenseItem
-            title={props.items[2].title}
-            amount={props.items[2].amount}
-            date={props.items[2].date}
-            />
-        </div>
+        <>
+            {/* <div className="expenses">
+                <ExpenseItem
+                title={props.items[0].title}
+                amount={props.items[0].amount}
+                date={props.items[0].date}
+                />
+                <ExpenseItem
+                title={props.items[1].title}
+                amount={props.items[1].amount}
+                date={props.items[1].date}
+                />
+                <ExpenseItem
+                title={props.items[2].title}
+                amount={props.items[2].amount}
+                date={props.items[2].date}
+                />
+            </div> */}
+            <div className="expenses">
+              {
+                  props.items.map(item => (
+                      <ExpenseItem
+                        title={item.title}
+                        amount={item.amount}
+                        date={item.date}
+                      />
+                ))
+              }  
+            </div>
+        </>
+       
     )
 }
 
